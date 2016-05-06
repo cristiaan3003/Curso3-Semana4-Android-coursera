ea305f4711df2c2f3940df3d9e14bb612df013de
cristian <cristiaan3003@gmail.com>
2016-05-06 02:21:46 -0300

    fisrt commit

---
+++ app/src/main/java/com/cristian/fragment/fragments/PrefilFragment.java
@@ -0,0 +1,70 @@
+package com.cristian.fragment.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.cristian.fragment.R;
+import com.cristian.fragment.adapter.RVAdapter;
+import com.cristian.fragment.pojo.Mascota;
+
+import java.util.ArrayList;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PrefilFragment extends Fragment {
+
+    ArrayList mascotas;
+    RecyclerView rvPerfil;
+    TextView nombre_perro;
+    public PrefilFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =  inflater.inflate(R.layout.fragment_prefil, container, false);
+
+
+        //ADAPTER
+        //---------------------------------------------------------------------------------
+        mascotas = new ArrayList<Mascota>();
+        mascotas.add(new Mascota("",10,R.drawable.perro1));
+        mascotas.add(new Mascota("",9,R.drawable.perro1));
+        mascotas.add(new Mascota("",8,R.drawable.perro1));
+        mascotas.add(new Mascota("",7,R.drawable.perro1));
+        mascotas.add(new Mascota("",6,R.drawable.perro1));
+        mascotas.add(new Mascota("",5,R.drawable.perro1));
+        mascotas.add(new Mascota("", 4, R.drawable.perro1));
+        mascotas.add(new Mascota("",3,R.drawable.perro1));
+        mascotas.add(new Mascota("", 0, R.drawable.perro1));
+
+
+        rvPerfil = (RecyclerView) v.findViewById(R.id.rv_perfil);
+        rvPerfil.setHasFixedSize(true);
+        final GridLayoutManager glm= new GridLayoutManager(getActivity(),3,GridLayoutManager.VERTICAL,false);
+        rvPerfil.setLayoutManager(glm);
+        RVAdapter adapter = new RVAdapter(mascotas);
+        rvPerfil.setAdapter(adapter);
+
+
+        //FIN ADAPTER
+        //-------------------------------------------------------------------------------------
+
+        return v;
+    }
+
+}
